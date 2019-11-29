49f221cf76a7d0364596fd75a2727d7da3e75160
Daniel Pinheiro <danielpm1982@gmail.com>
2019-11-29 16:55:04 -0300

    .

---
+++ SOAP_WS1/src/main/java/com/danielpm1982/SOAP_WS1/config/WebServiceConfig.java
@@ -0,0 +1,23 @@
+package com.danielpm1982.SOAP_WS1.config;
+import javax.xml.ws.Endpoint;
+import org.apache.cxf.Bus;
+import org.apache.cxf.jaxws.EndpointImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import com.danielpm1982.SOAP_WS1.ws.CustomerOrdersWSImpl;
+
+@Configuration
+public class WebServiceConfig {
+	@Autowired
+	private Bus bus;
+	@Bean
+	public Endpoint endpoint() {
+		Endpoint endpoint = new EndpointImpl(bus, new CustomerOrdersWSImpl());
+		endpoint.publish("/customerOrdersService");
+		return endpoint;
+	}
+}
+
+//This is the publishing of the Endpoint of the webService along with its url
+//and PortType implementation, so that it can be accessed from the consumer.
